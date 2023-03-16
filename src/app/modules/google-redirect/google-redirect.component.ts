6fcb5c9272438f12432d8d5d89c93ba337cf2883
Andrii <artis_hire@ukr.net>
2023-03-16 11:53:09 +0200

    Add save token and redirect to main page

--- src/app/modules/google-redirect/google-redirect.component.ts
+++ src/app/modules/google-redirect/google-redirect.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService, ITokens } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-google-redirect',
   templateUrl: './google-redirect.component.html',
-  styleUrls: ['./google-redirect.component.scss']
+  styleUrls: ['./google-redirect.component.scss'],
 })
-export class GoogleRedirectComponent {
+export class GoogleRedirectComponent implements OnInit {
+  constructor(
+    private route: ActivatedRoute,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
+  ngOnInit(): void {
+    const tokens = this.route.snapshot.queryParams as ITokens;
+    this.authService.login(tokens);
+    this.router.navigate(['']);
+  }
 }
--- src/app/modules/google-redirect/google-redirect.module.ts
+++ src/app/modules/google-redirect/google-redirect.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GoogleRedirectComponent } from './google-redirect.component';
 import { GoogleRedirectRoutingModule } from './google-redirect-routing.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [GoogleRedirectComponent],
-  imports: [CommonModule, GoogleRedirectRoutingModule],
+  imports: [CommonModule, GoogleRedirectRoutingModule, SharedModule],
 })
 export class GoogleRedirectModule {}
